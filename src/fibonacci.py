a16e347303f5b51082786967eae95eb4ff3c5d4d
im <poson_hemp@yahoo.co.jp>
2022-08-19 17:20:20 +0900

    update fibonacci function (with pattern match)

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,10 +1,11 @@
 def fibonacci(num):
-    if num == 0:
-        return 1
-    elif num == 1:
-        return 1
-    else:
-        return fibonacci(num - 1) + fibonacci(num - 2)
+    match num:
+        case 0:
+            return 1
+        case 1:
+            return 1
+        case _:
+            return fibonacci(num - 1) + fibonacci(num - 2)
 
 
 for i in range(10):
