575fcecbe7c087056ca3cb136c69dc16a60729c1
im <poson_hemp@yahoo.co.jp>
2022-08-31 05:32:00 +0900

    chore/ remove def __init__(self): self.main()

--- src/computational_complexity/fizzbuzz.py
+++ src/computational_complexity/fizzbuzz.py
@@ -2,10 +2,6 @@ class FizzBuzz:
     LIMIT_NUMBER: int = 100
 
 
-    def __init__(self):
-        self.main()
-
-
     def main(self) -> None:
         for i in range(1, self.LIMIT_NUMBER + 1):
             if (i % 3 == 0) and (i % 4 == 0):
@@ -19,4 +15,4 @@ class FizzBuzz:
 
 
 if __name__ == '__main__':
-    FizzBuzz()
+    FizzBuzz().main()
