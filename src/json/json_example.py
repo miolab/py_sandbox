a99f305911dbb1d5efb867bcbed4736b02cd013f
im <poson_hemp@yahoo.co.jp>
2022-11-09 08:10:52 +0900

    chore/ add function docs

--- src/json/json_example.py
+++ src/json/json_example.py
@@ -15,7 +15,14 @@ print(json.dumps(fruits))
 
 
 def gen_json(path: str) -> None:
-    """Generate new JSON file
+    """Generate new JSON file.
+
+    example:
+    - execute
+        - `gen_json('src/json/sample.json')`
+    - result
+        - a new JSON file generated as
+        `{"fruits": [{"id": 111, "name": "apple"}, {"id": 222, "name": "orange"}]}`
     """
     with open(path, 'w') as f:
         json.dump(fruits, f)
