9ae1b62e05a9b28355d2457a2294aa29d25a0ace
im <poson_hemp@yahoo.co.jp>
2022-11-17 06:07:51 +0900

    feat/ example GET request
    result
    ```
    python src/rest_api/request_sample.py
    
    200
    [
      {
        "userId": 1,
        "id": 1,
        "title": "delectus aut autem",
        "completed": false
      }
    ]
    [{'userId': 1, 'id': 1, 'title': 'delectus aut autem', 'completed': False}]
    ```

--- src/rest_api/request_sample.py
+++ src/rest_api/request_sample.py
@@ -2,6 +2,15 @@ import requests
 
 URL = 'https://jsonplaceholder.typicode.com'
 
-r = requests.get(f'{URL}/todos/1')
+payload = {
+    'id': 1
+}
+
+r = requests.get(
+    f'{URL}/todos',
+    params=payload
+)
 
 print(r.status_code)
+print(r.text)
+print(r.json())
