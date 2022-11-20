a6d1d7e2708b0816d5dbf9425296d6443593f232
im <poson_hemp@yahoo.co.jp>
2022-11-21 05:47:01 +0900

    feat/ example POST request
    - result
    ```
    src/rest_api/request_sample.py
    
    <class 'requests.models.Response'>
    201
    {
      "id": 101,
      "title": "sample title",
      "body": "sample body"
    }
    {'id': 101, 'title': 'sample title', 'body': 'sample body'}
    ```

--- src/rest_api/request_sample.py
+++ src/rest_api/request_sample.py
@@ -4,10 +4,13 @@ URL = 'https://jsonplaceholder.typicode.com/'
 
 
 def main():
-    return _get()
+    # return _get()
+    return _post()
 
 
 def _get() -> requests.models.Response:
+    """Execute GET request.
+    """
     route = 'posts'
     payload = {
         'id': 1
@@ -16,6 +19,21 @@ def _get() -> requests.models.Response:
     return requests.get(
         f'{URL}{route}',
         params=payload
+    )
+
+
+def _post() -> requests.models.Response:
+    """Execute mock POST request.
+    """
+    route = 'posts'
+    payload = {
+        'id': 1,
+        'title': 'sample title',
+        'body': 'sample body'
+    }
+    return requests.post(
+        f'{URL}{route}',
+        data=payload
     )
 
 
