caa6aed745a7312cb9f1815878ce9c9038cecb0f
im <poson_hemp@yahoo.co.jp>
2022-11-21 09:49:27 +0900

    feat/ example DELETE request
    - result
    ```
    python src/rest_api/request_sample.py
    
    <class 'requests.models.Response'>
    200
    {}
    {}
    ```

--- src/rest_api/request_sample.py
+++ src/rest_api/request_sample.py
@@ -5,7 +5,8 @@ URL = 'https://jsonplaceholder.typicode.com/'
 
 def main():
     # return _get()
-    return _post()
+    # return _post()
+    return _delete()
 
 
 def _get() -> requests.models.Response:
@@ -32,6 +33,19 @@ def _post() -> requests.models.Response:
         'body': 'sample body'
     }
     return requests.post(
+        f'{URL}{route}',
+        data=payload
+    )
+
+
+def _delete() -> requests.models.Response:
+    """Execute mock DELETE request.
+    """
+    route = 'posts/1'
+    payload = {
+        'id': 1
+    }
+    return requests.delete(
         f'{URL}{route}',
         data=payload
     )
