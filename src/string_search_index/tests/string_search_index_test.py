ff6294ad0194322ae95073a99320ad3de2708be5
im <poson_hemp@yahoo.co.jp>
2023-08-15 12:03:55 +0900

    add/ tests (brute_force_search_index, boyer_moore_search)

--- src/string_search_index/tests/string_search_index_test.py
+++ src/string_search_index/tests/string_search_index_test.py
@@ -3,5 +3,20 @@ from string_search_index import StringSearchIndex
 
 
 def test_brute_force_search_index__normal_case():
-    """Normal case tests."""
-    assert StringSearchIndex.brute_force_search_index("abcdef", "bc") == 1
+    # Normal case tests.
+    assert StringSearchIndex.brute_force_search_index("abcdefg", "bc") == 1
+    assert StringSearchIndex.brute_force_search_index("Hello world", "wor") == 6
+
+    # Target substring does NOT exist case tests.
+    assert StringSearchIndex.brute_force_search_index("abc", "xyz") == -1
+    assert StringSearchIndex.brute_force_search_index("abc", "abcde") == -1
+
+
+def test_boyer_moore_search_index__normal_case():
+    # Normal case tests.
+    assert StringSearchIndex.boyer_moore_search("abcdefg", "bc") == 1
+    assert StringSearchIndex.boyer_moore_search("Hello world", "wor") == 6
+
+    # Target substring does NOT exist case tests.
+    assert StringSearchIndex.boyer_moore_search("abc", "xyz") == -1
+    assert StringSearchIndex.boyer_moore_search("abc", "abcde") == -1
